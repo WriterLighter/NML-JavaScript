2cb4a79bbccd9b9fd4471ba8187c0dfecb6bd562
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-05-14 23:50:26 +0900

    [add] add mode

--- bundle.js
+++ bundle.js
@@ -4,10 +4,14 @@ NML = require("./out/nml")
 window.convert = function() {
 
   var textarea = document.getElementsByTagName('textarea')[0];
+  var mode = document.getElementById('mode');
 
   var novel = new NML(textarea.value)
   var output = document.getElementById("output");
-  output.innerHTML = novel.to("html");
+
+  alert(mode.options[mode.selectedIndex].value);
+
+  output.innerHTML = novel.to(mode.options[mode.selectedIndex].value);
 }
 
 },{"./out/nml":2}],2:[function(require,module,exports){
--- main.js
+++ main.js
@@ -3,8 +3,10 @@ NML = require("./out/nml")
 window.convert = function() {
 
   var textarea = document.getElementsByTagName('textarea')[0];
+  var mode = document.getElementById('mode');
 
   var novel = new NML(textarea.value)
   var output = document.getElementById("output");
-  output.innerHTML = novel.to("html");
+
+  output.innerHTML = novel.to(mode.options[mode.selectedIndex].value);
 }
