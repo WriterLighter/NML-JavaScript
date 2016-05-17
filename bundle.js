c96b54b827075d25c041e6dd9ca1c3cc354a468b
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-05-17 23:57:58 +0900

    Revert "[fix] fixed #3"
    This reverts commit 301fe26d0bb603cdba989f8297eee9da1b3e0205.

--- bundle.js
+++ bundle.js
@@ -9,93 +9,14 @@ window.convert = function() {
   var novel = new NML(textarea.value)
   var output = document.getElementById("output");
 
-  if (mode.options[mode.selectedIndex].value === "htmlpre"){
-    output.innerHTML = novel.to("html");
-  }else if (mode.options[mode.selectedIndex].value === "html"){
-    var text = novel.to(mode.options[mode.selectedIndex].value);
-    text = text.replace( /\n/g , "" ) ;
-    output.innerHTML = "<pre>" + format_xml(text) + "</pre>";
-
-    var pre = document.querySelectorAll('pre');
-    for(var i = 0; i < pre.length; i++) {
-      pre[i].innerHTML = escapeHtml(pre[i].innerHTML);
-    }
-  }else{
+  if (mode.options[mode.selectedIndex].value === "plain"){
     output.innerHTML = "<pre>" + novel.to(mode.options[mode.selectedIndex].value) + "</pre>";
+  }else{
+    output.innerHTML = novel.to(mode.options[mode.selectedIndex].value);
   }
+
+
 }
-function spaces(len)
-{
-	var s = '';
-	var indent = len*4;
-	for (i=0;i<indent;i++) {s += " ";}
-
-	return s;
-}
-
-function format_xml(str)
-{
-	var xml = '';
-
-	// add newlines
-	str = str.replace(/(>)(<)(\/*)/g,"$1\r$2$3");
-
-	// add indents
-	var pad = 0;
-	var indent;
-	var node;
-
-	// split the string
-	var strArr = str.split("\r");
-
-	// check the various tag states
-	for (var i = 0; i < strArr.length; i++) {
-		indent = 0;
-		node = strArr[i];
-
-		if(node.match(/.+<\/\w[^>]*>$/)){ //open and closing in the same line
-			indent = 0;
-		} else if(node.match(/^<\/\w/)){ // closing tag
-			if (pad > 0){pad -= 1;}
-		} else if (node.match(/^<\w[^>]*[^\/]>.*$/)){ //opening tag
-			indent = 1;
-		} else
-			indent = 0;
-		//}
-
-		xml += spaces(pad) + node + "\r";
-		pad += indent;
-	}
-
-	return xml;
-}
-
-var escapeHtml = (function (String) {
-  var escapeMap = {
-    '&': '&amp;',
-    "'": '&#x27;',
-    '`': '&#x60;',
-    '"': '&quot;',
-    '<': '&lt;',
-    '>': '&gt;'
-  };
-  var escapeReg = '[';
-  var reg;
-  for (var p in escapeMap) {
-    if (escapeMap.hasOwnProperty(p)) {
-      escapeReg += p;
-    }
-  }
-  escapeReg += ']';
-  reg = new RegExp(escapeReg, 'g');
-  return function escapeHtml (str) {
-    str = (str === null || str === undefined) ? '' : '' + str;
-    return str.replace(reg, function (match) {
-      return escapeMap[match];
-    });
-  };
-}(String));
-
 
 $(function(){
   convert();
@@ -110,13 +31,9 @@ $(function(){
 },{"./out/nml":2}],2:[function(require,module,exports){
 (function() {
   module.exports = this.NML = (function() {
-    var checkBlockquotes, checkBold, checkItalic, checkLine, checkLink, checkNewPage, checkNextLine, checkPunctuationNumber, checkPunctuationSymbol, checkReturn, checkRuby, checkSharp, checkSpace, checkStrikethrough, mode, multi_line, multi_line_mode;
+    var checkBlockquotes, checkBold, checkItalic, checkLine, checkLink, checkNewPage, checkNextLine, checkPunctuationNumber, checkPunctuationSymbol, checkReturn, checkRuby, checkSharp, checkSpace, checkStrikethrough, mode;
 
     mode = 0;
-
-    multi_line = "";
-
-    multi_line_mode = 0;
 
     function NML(text1) {
       this.text = text1;
@@ -303,13 +220,13 @@ $(function(){
               linkText = linkText.substring(1, linkText.length - 1);
               url = url.substring(1, url.length - 1);
               name = name.substring(1, name.length - 1);
-              line = line.replace(text, '<img src="' + url + '" alt="' + linkText + '" title="' + name + '"/>');
+              line = line.replace(text, '<img src="' + url + '" alt="' + linkText + '" title="' + name + '">');
             } else {
               linkText = line.match(/\[.*?\]/)[0];
               url = line.match(/[\((].*?[\))]/)[0].replace(RegExp(' ', 'g'), '');
               linkText = linkText.substring(1, linkText.length - 1);
               url = url.substring(1, url.length - 1);
-              line = line.replace(text, '<img src="' + url + '" alt="' + linkText + '"/>');
+              line = line.replace(text, '<img src="' + url + '" alt="' + linkText + '">');
             }
           } else if (mode === 2) {
             line = line.replace(text, "");
@@ -342,7 +259,7 @@ $(function(){
     checkPunctuationNumber = function(line) {
       if (line.match(/^[0-9]+\.$/)) {
         if (mode === 1) {
-          line = '<hr class="number"/>';
+          line = '<hr class="number">';
         }
       }
       return line;
@@ -351,7 +268,7 @@ $(function(){
     checkPunctuationSymbol = function(line) {
       if (line.match(/^[-*ー*]+\.$/)) {
         if (mode === 1) {
-          line = '<hr class="symbol"/>';
+          line = '<hr class="symbol">';
         }
       }
       return line;
@@ -359,7 +276,7 @@ $(function(){
 
     checkNextLine = function(line) {
       if (line.match(/[ \s]{3,}$/)) {
-        line = line + "<br/>";
+        line = line + "<br>";
       }
       return line;
     };
